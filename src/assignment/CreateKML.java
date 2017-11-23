0992b2e1866d6ca9f55986187981f602be26a1f6
Isabella Oren <Isabella Oren@LAPTOP-R6MH9MMA>
2017-11-23 19:12:11 +0200

    The create KML class is now working

--- src/assignment/CreateKML.java
+++ src/assignment/CreateKML.java
@@ -1,117 +1,79 @@
 package assignment;
 
 import java.io.*;
+import java.sql.Timestamp;
 import java.util.*;
 import java.util.function.Predicate;
 
+import javax.print.Doc;
+
+
+import de.micromata.opengis.kml.v_2_2_0.Document;
+import de.micromata.opengis.kml.v_2_2_0.Kml;
+import de.micromata.opengis.kml.v_2_2_0.KmlFactory;
+import de.micromata.opengis.kml.v_2_2_0.Placemark;
+
 public class CreateKML {
-	public static void main(String[] args) throws FileNotFoundException {
-	Kml kml = KmlFactory.createKml();
-	String text = "2011-10-02 18:48:05";
-	Timestamp ts = Timestamp.valueOf(text);
-	Placemark p = KmlFactory.createPlacemark();
-	p.setName("uno");
-	p.setVisibility(true);
-	p.setOpen(false);
-	p.createAndSetTimeStamp().addToTimeStampSimpleExtension(ts);
-	p.createAndSetPoint().addToCoordinates(32.957366,34.143567);
-	Placemark p2 = KmlFactory.createPlacemark();
-	p2.setName("dos");
-	p2.setVisibility(true);
-	p2.setOpen(false);
-	p2.createAndSetTimeStamp().addToTimeStampSimpleExtension(ts);
-	p2.createAndSetPoint().addToCoordinates(31.957366,33.143567);
-	de.micromata.opengis.kml.v_2_2_0.Document doc = kml.createAndSetDocument().withName("MyMarkers");
-	kml.setFeature(doc);
-	kml.marshal(new File("C:/Users/Isabella Oren/Desktop/Htest2.kml"));
-	}
+	private static String Id ="";
+	private static Double Lat =0.0;
+	private static Double Lon =0.0;
+	private static Double Alt =0.0;
+
 	
-/*
-	private static List<ArrayList<String>> currentcsv=new ArrayList<ArrayList<String>> ();
-
-
-
-	public static void main(String[] args) {
-		toKML();
+	public static void main(String[] args) throws FileNotFoundException {
+	createTheKmlFile();
 	}
-		public static void toKML() {
-		String csvFile = "C:/Users/arbel/Desktop/test2.csv";
-		BufferedReader br = null;
-		//next string is line
-		String l = "";
-		//next string is the sign for us to split
-		String split = ",";
+	//creates a KML file with a timeline
+	public static void createTheKmlFile() {
+		final Kml kml=new Kml();
+		de.micromata.opengis.kml.v_2_2_0.Document doc = kml.createAndSetDocument();
 		try {
-			br = new BufferedReader(new FileReader(csvFile));
 			
-			// thw while works as long as we have something to read from in the array list
-			while ((l = br.readLine()) != null) {
-				List<String> in = Arrays.asList(l.split(split));
-				ArrayList<String> inner=new ArrayList<String>(in);
-				//שולח לפונקציה שבודקת אם בשורה יש תא ריק או הסימן & שמשגע
-				//את הקובץ kml
-				if(checkValid(inner))
-					currentcsv.add(inner);
-			}
-			//creating a kml file for the data filtered by the id:
-			//if we want to use a filter we need to change the next string:
-			String filter="id";
-			changeByFilter(filter, currentcsv);
+			FileReader filereader = new FileReader("C:\\Users\\Isabella Oren\\git\\Wifi_Mapping\\ScanInput\\csvToKml.csv");
+			BufferedReader br = new BufferedReader(filereader);
+			String line;
+			//the first line in the file is not relevant thats why we read it and then dont use it but override it.
+			line = br.readLine();
+			line = br.readLine();
+			List <String> neededLine= Arrays.asList(line.split(","));
+			String time = neededLine.get(0);
+			String id = neededLine.get(1);
+			String lat = neededLine.get(2);
+			String lon = neededLine.get(3);
+			String alt = neededLine.get(4);
 
-			FileWriter writer2 = new FileWriter("C:/Users/arbel/Desktop/test3.kml");
-			String kmlstart="<?xml version=\"1.0\" encoding=\"UTF-8\"?>   ";
-			String kmlstart2="<kml xmlns=\"http://earth.google.com/kml/2.0\">   ";
-			String kmlstart3="<Document>   ";
-			writer2.write(kmlstart+"\n");
-			writer2.write(kmlstart2+"\n");
-			writer2.write(kmlstart3+"\n");
-			String placment="<Placemark>  ";
-			String closeplacement="</Placemark>";
-			for(int k=1; k<currentcsv.size(); k++)
-			{
-				writer2.write(placment+"\n");
-				writer2.write("<name>"+currentcsv.get(k).get(1)+"</name>"+"\n");
-				// העתקתי את התבנית לתיאור מקובץ קיי-אמ-אל מוכן ועכשיו הוא נותן את כל השדות הפרטיים של הנטוורק הראשון
-				writer2.write("<description>"+"BSSID: <b>"+currentcsv.get(k).get(6)+"</b><br/>Mac: <b>"+currentcsv.get(k).get(7)+"</b><br/>Frequency: <b>"+currentcsv.get(k).get(8)+"</b><br/>signal: <b>"+currentcsv.get(k).get(9)+"</b></description>\n");
-				//השדות כאן היו מבולבלים, רק סידרתי לפי הטבלה הסופית שלנו
-				writer2.write("<Point><coordinates>"+currentcsv.get(k).get(3)+","+currentcsv.get(k).get(2)+","+currentcsv.get(k).get(4)+"</coordinates></Point>"+"\n");
-				writer2.write(closeplacement+"\n");
-			}
-			String kmlclose2="</Document>";
-			String kmlclose3="</kml>";
-			writer2.write(kmlclose2+"\n");
-			writer2.write(kmlclose3+"\n");
-			writer2.close();
+			while(line != null){
+				Id= id;
+				Lat = Double.parseDouble(lat);
+				Lon = Double.parseDouble(lon);
+				Alt = Double.parseDouble(alt);
+				List <String> theDataArray= Arrays.asList(line.split(","));
+				time = theDataArray.get(0);
+				id = theDataArray.get(1);
+				lat = theDataArray.get(2);
+				lon = theDataArray.get(3);
+				alt = theDataArray.get(4);
+				time = time.replace(' ','T');
+				time = time+'Z';
+				Placemark place = KmlFactory.createPlacemark();
+				place.createAndSetTimeStamp().withWhen(time);
+				String location=lon+","+lat+","+alt;
 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+				doc.createAndAddPlacemark().withName(Id).withTimePrimitive(place.getTimePrimitive())
+				.createAndSetPoint().addToCoordinates(location);
+
+				line = br.readLine();
 			}
+			line = br.readLine();
+			line = br.readLine();
+			br.close();
+			filereader.close();
+			kml.marshal(new File("C:\\Users\\Isabella Oren\\git\\Wifi_Mapping\\ScanInput\\kml.kml"));
 		}
-
+		catch(IOException ex) {
+			System.out.print("Error reading file\n" + ex);
+			System.exit(2);
+		}
 	}
-	private static void changeByFilter(String filter, List<ArrayList<String>> currentcsv2) {
-			// TODO Auto-generated method stub
-			
-		}
-	public static boolean checkValid(ArrayList<String> arr){
-		boolean flag =true;
-		for(int i=0;i<arr.size();i++){
-			String check=arr.get(i);
-			if(check==""||check.contains("&")){
-				flag=false;
-			}
-		}
 
-		return flag;
-	}
-	*/
 }
