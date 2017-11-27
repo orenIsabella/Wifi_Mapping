05c294ee089dde05d1bb0fba41e06e377027ccce
Isabella Oren <Isabella Oren@LAPTOP-R6MH9MMA>
2017-11-27 13:58:36 +0200

    filter adding

--- src/assignment/Filter.java
+++ src/assignment/Filter.java
@@ -5,20 +5,20 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Filter {
-	private static void changeByFilter(String filter, List<ArrayList<String>> currentcsv){
+	private static void changeByFilter(String filter, List<ArrayList<String>> currentcsv, String theFilter){
 		if(filter.equals("id")){
-			Predicate<ArrayList<String>> id=s -> s.get(1).contains("OnePlus3T");
+			Predicate<ArrayList<String>> id=s -> s.get(1).contains(theFilter);
 			List<ArrayList<String>> afterFilter=filterby(currentcsv ,id);
 			currentcsv=afterFilter;
 			//System.out.println(afterFilter.size());
 		}
 		if(filter.equals("time")){
-			Predicate<ArrayList<String>> time=s -> s.get(0).contains("00:00");
+			Predicate<ArrayList<String>> time=s -> s.get(0).contains(theFilter);
 			List<ArrayList<String>> afterFilter=filterby(currentcsv ,time);	
 			currentcsv=afterFilter;
 		}
 		if(filter.equals("location")){
-			Predicate<ArrayList<String>> location=s -> s.get(1).contains("London");
+			Predicate<ArrayList<String>> location=s -> s.get(1).contains(theFilter);
 			List<ArrayList<String>> afterFilter=filterby(currentcsv ,location);	
 			currentcsv=afterFilter;
 		}
@@ -32,7 +32,7 @@ public class Filter {
 		for(int i=1; i<data.size(); i++)
 		{
 			System.out.println("v");
-			if(term.test(data.get(i))==false)
+			if(term.test(data.get(i))==true)
 			{
 				out.add(data.get(i));
 			}
