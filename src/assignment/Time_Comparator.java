2639cd5a23176d4efb316966e32fb92645140e6d
arbel <arbel@Lenovo-PC>
2017-12-07 16:00:11 +0200

    compatible to 'Network' object

--- src/assignment/Time_Comparator.java
+++ src/assignment/Time_Comparator.java
@@ -5,12 +5,12 @@ import java.util.Comparator;
 
 
 
-public class Time_Comparator implements Comparator<ArrayList<String>>  {
+public class Time_Comparator implements Comparator<Network>  {
 
 	@Override
-	public int compare(ArrayList<String> arg0, ArrayList<String> arg1) {
-		String [] data0=arg0.get(0).split(" ");
-		String [] data1=arg1.get(0).split(" ");
+	public int compare(Network arg0, Network arg1) {
+		String [] data0=arg0.getTime().split(" ");
+		String [] data1=arg1.getTime().split(" ");
 		int ans= data0[0].compareTo(data1[0]);
 		if(ans==0)
 			ans=data0[1].compareTo(data1[1]);
