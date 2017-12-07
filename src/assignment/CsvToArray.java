48b7b6e7162508a0627a2b6493fd10c0d0e5d809
arbel <arbel@Lenovo-PC>
2017-12-07 14:44:31 +0200

    changed to be compatible with objects

--- src/assignment/CsvToArray.java
+++ src/assignment/CsvToArray.java
@@ -47,19 +47,12 @@ public class CsvToArray {
 				String currentLine;
 				currentLine = br.readLine();
 				List<String> model=Arrays.asList(currentLine.split(","));
-				model=Arrays.asList(model.get(4).split("="));
+				nets.addId(model);
 				currentLine = br.readLine();
 				currentLine = br.readLine();
 				while(currentLine!=null){
-					List<String> temp1=Arrays.asList(currentLine.split(","));
-					ArrayList<String> temp2=new ArrayList<>(temp1);
-					ArrayList<String> temp3=new ArrayList<>();
-					temp2.add(model.get(1));
-					ChanToFreq(temp2);
-					temp3=By_Order(temp2);
-					if(chackValid(temp3)){
-					All_Networks.add(temp3);
-					}
+					List<String> temp=Arrays.asList(currentLine.split(","));
+					nets.add(temp);
 					currentLine = br.readLine();
 				}
 				br.close();
@@ -73,45 +66,17 @@ public class CsvToArray {
 	
 	}
 	
-	public  ArrayList<ArrayList<String>> getAll_Networks() {
-		return All_Networks;
+	
+
+	public All_Networks getNets() {
+		return nets;
 	}
+
+
+
 
 	// ************** Private *******************
-	private static ArrayList<ArrayList<String>> All_Networks=new ArrayList<ArrayList<String>>();
-	
-	
-	private static void ChanToFreq(ArrayList<String> arr){
-		if(Integer.parseInt(arr.get(4))!=0)
-			if(Integer.parseInt(arr.get(4))<12)
-				arr.set(4, "2.4Ghz");
-			else arr.set(4, "5Ghz");
-	}
-
-	private static ArrayList<String> By_Order(ArrayList<String> arr){
-		ArrayList<String> Final=new ArrayList<>();
-		Final.add(arr.get(3));
-		Final.add(arr.get(11));
-		Final.add(arr.get(6));
-		Final.add(arr.get(7));
-		Final.add(arr.get(8));
-		Final.add(arr.get(1));
-		Final.add(arr.get(0));
-		Final.add(arr.get(4));
-		Final.add(arr.get(5));
-		return Final;
-	}
-	private static boolean chackValid(ArrayList<String> temp2) {
-		boolean flag=true;
-		if(temp2.size()!=9||!(temp2.get(6).contains(":"))||temp2.get(0).length()!=19)
-			flag=false;
-		for(int i=0;i<temp2.size();i++)
-		if(temp2.get(i).isEmpty())
-			flag=false;
-		
-		return flag;
-	}
-	
+	private All_Networks nets =new All_Networks();
 	
 	
 	}
