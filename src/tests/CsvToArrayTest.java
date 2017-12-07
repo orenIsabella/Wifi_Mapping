7dd09205f47de5603e2cc1b4c0bbb0e56db10230
arbel <arbel@Lenovo-PC>
2017-12-07 14:45:06 +0200

    changed to be compatible with new CsvToArray

--- src/tests/CsvToArrayTest.java
+++ src/tests/CsvToArrayTest.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import org.junit.Test;
 
 import assignment.CsvToArray;
+import assignment.Network;
 
 
 public class CsvToArrayTest {
@@ -16,58 +17,36 @@ public class CsvToArrayTest {
 	public void testCsvToArray() {
 		CsvToArray Array=new CsvToArray();
 		Array.csvToArray("ScanInput");
-		ArrayList<ArrayList<String>> arr_test =Array.getAll_Networks();
-		for(int i=1;i<arr_test.size();i++)
-			for(int j=0;j<arr_test.get(i).size();j++){
-				switch(j){
-				case 0:	if(arr_test.get(i).get(j).length()!=19)
-					fail("time is not correct");
-				continue;
-				case 1: if(arr_test.get(i).get(j).contains("&")||arr_test.get(i).get(j).isEmpty())
-					fail("ID is bad or missing");
-				continue;
-				case 2: 
-					if(arr_test.get(i).get(j).matches(".*[a-z].*"))
-						fail("Lat is bad");
-					double lat=Double.parseDouble(arr_test.get(i).get(j));
-					if(lat>90||lat<0)
-						fail("Lat is bad");
-					continue;
-				case 3:
-					if(arr_test.get(i).get(j).matches(".*[a-z].*"))
-						fail("Lon is bad");
-					double lon=Double.parseDouble(arr_test.get(i).get(j));
-					if(lon>180||lon<0)
-						fail("Lon is bad");
-					continue;
-				case 4:
-					if(arr_test.get(i).get(j).matches(".*[a-z].*"))
-						fail("Alt is bad");
-					continue;
-				case 5:
-					if(arr_test.get(i).get(j).isEmpty())
-						fail("bad SSID");
-					continue;
-				case 6:
-					if(arr_test.get(i).get(j).matches(".*[g-z].*"))
-						fail("bad mac-not hexa");
-					if(arr_test.get(i).get(j).length()!=17)
-						fail("bad mac!");
-					continue;
-				case 7:
-					if(!(arr_test.get(i).get(j).equals("2.4Ghz")||arr_test.get(i).get(j).equals("5Ghz")))
-						fail("bad freq");
-					continue;
-				case 8:
-					int signal=Integer.parseInt(arr_test.get(i).get(j));
-					if(signal<=-113||signal>=0)
-						fail("bad signal");
-					continue;
-				default:
-					fail("too many fields");
-				}
-			}
+		ArrayList<Network> arr_test =Array.getNets().getNetworks();
 
-	}
+		for(int j=0;j<arr_test.size();j++){
+
+			if(arr_test.get(j).getTime().length()!=19)
+				fail("time is not correct");
+			if(arr_test.get(j).getId().contains("&")||arr_test.get(j).getId().isEmpty())
+				fail("ID is bad or missing");
+			double lat=arr_test.get(j).getLat();
+			if(lat>90||lat<0)
+				fail("Lat is bad");
+			double lon=arr_test.get(j).getLon();
+			if(lon>180||lon<0)
+				fail("Lon is bad");
+			double Alt=arr_test.get(j).getAlt();
+			if(Alt>2000)
+				fail("Alt is bad");
+			if(arr_test.get(j).getSsid().isEmpty())
+				fail("bad SSID");
+			if(arr_test.get(j).getMac().matches(".*[g-z].*"))
+				fail("bad mac-not hexa");
+			if(arr_test.get(j).getMac().length()!=17)
+				fail("bad mac!");
+			if(!(arr_test.get(j).getFreq()==2.4||arr_test.get(j).getFreq()==5))
+				fail("bad freq");
+			int signal=arr_test.get(j).getSign();
+			if(signal<=-113||signal>=0)
+				fail("bad signal");
+		}
+	
+}
 
 }
