c0f1e7a9a101367fbef347b665971a7b1c4d7de9
arbel <arbel@Lenovo-PC>
2017-12-08 12:13:00 +0200

    junit for scanOrder (old final_networks)

---
+++ src/tests/ScanOrderTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import assignment.ScanOrder;
+
+public class ScanOrderTest {
+
+	@Test
+	public void testFinal_order() {
+		ScanOrder Array=new ScanOrder("ScanInput","ScanInput/csvtest.csv");	
+		Array.final_order();
+		ArrayList<ArrayList<String>> test=Array.getFinal_Networks();
+		for(int i=0;i<test.size()-1;i++){
+			ArrayList<String> checkedLine=test.get(i);
+			for(int j=i+1;j<test.size();j++){
+				ArrayList<String> currentLine=test.get(j);
+				if(currentLine.get(0)==checkedLine.get(0)
+						&&currentLine.get(1)==checkedLine.get(1))
+					fail("i: "+i+" j: " +j+" Same point!");
+				if(checkedLine.size()>46)
+					fail("too long line!!");
+			}
+		}	
+		
+	}
+
+}
