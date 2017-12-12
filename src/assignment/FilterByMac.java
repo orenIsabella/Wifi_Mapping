0e6438a376bde7e1c31eb041024c5de1b8565270
arbel <arbel@Lenovo-PC>
2017-12-12 14:15:19 +0200

    changed parameters

--- src/assignment/FilterByMac.java
+++ src/assignment/FilterByMac.java
@@ -2,9 +2,9 @@ package assignment;
 
 public class FilterByMac {
 	
-	All_Networks nets=new All_Networks("ScanInput");
+	//All_Networks nets=new All_Networks("ScanInput");
 	
-	public void filter(String mac) {
+	public void filter(String mac,All_Networks nets) {
 		int place=0;
 		while(place<nets.size()) {
 			if(!(nets.getNetworks().get(place).getMac().equals(mac))) {
