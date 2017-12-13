bec73f74e92592d6c31e038f1c05c92ee38af024
arbel <arbel@Lenovo-PC>
2017-12-13 17:27:04 +0200

    add equals method

--- src/assignment/Point3D.java
+++ src/assignment/Point3D.java
@@ -1,50 +1,61 @@
 package assignment;
 
 public class Point3D {
-private double Lat,Lon,Alt;
+	private double Lat,Lon,Alt;
 
-public Point3D(double lan, double lon, double alt) {
-	super();
-	this.Lat = lan;
-	this.Lon = lon;
-	this.Alt = alt;
-}
+	public Point3D(double lan, double lon, double alt) {
+		super();
+		this.Lat = lan;
+		this.Lon = lon;
+		this.Alt = alt;
+	}
 
-public Point3D() {
-	this.Lat = -4000;
-	this.Lon = -4000;
-	this.Alt = -4000;
-}
+	public Point3D() {
+		this.Lat = -4000;
+		this.Lon = -4000;
+		this.Alt = -4000;
+	}
 
-public Point3D(Point3D a) {
-	super();
-	this.Lat = a.Lat;
-	this.Lon = a.Lon;
-	this.Alt = a.Alt;
-}
+	public Point3D(Point3D a) {
+		super();
+		this.Lat = a.Lat;
+		this.Lon = a.Lon;
+		this.Alt = a.Alt;
+	}
 
-public double getLat() {
-	return Lat;
-}
+	public double getLat() {
+		return Lat;
+	}
 
-public void setLat(double lat) {
-	this.Lat = lat;
-}
+	public void setLat(double lat) {
+		this.Lat = lat;
+	}
 
-public double getLon() {
-	return Lon;
-}
+	public double getLon() {
+		return Lon;
+	}
 
-public void setLon(double lon) {
-	this.Lon = lon;
-}
+	public void setLon(double lon) {
+		this.Lon = lon;
+	}
 
-public double getAlt() {
-	return Alt;
-}
+	public double getAlt() {
+		return Alt;
+	}
 
-public void setAlt(double alt) {
-	this.Alt = alt;
-}
+	public void setAlt(double alt) {
+		this.Alt = alt;
+	}
 
+	public boolean equals(Point3D a){
+		if(
+				this.Alt==a.Alt&&
+				this.Lat==a.Lat&&
+				this.Lon==a.Lon
+				)
+		{
+			return true;
+		}
+		else return false;
+	}
 }
