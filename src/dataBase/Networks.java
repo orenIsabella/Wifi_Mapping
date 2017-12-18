9a2bf9fdcd312b10776456b0aa0500f50f5116d3
arbel <arbel@Lenovo-PC>
2017-12-18 19:58:46 +0200

    change in method name

--- src/dataBase/Networks.java
+++ src/dataBase/Networks.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Networks {
 private ArrayList<A_Point> Networks=new ArrayList<A_Point>();
 
-public void AddFromInput(String path){
+public void addFromInput(String path){
 	Output2Networks a=new Output2Networks();
 	a.add(path, Networks);
 }
