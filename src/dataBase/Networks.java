ee254c87042da38c8ac48da6d96307b051bd295c
arbel <arbel@Lenovo-PC>
2017-12-19 15:47:50 +0200

    added copy constructor

--- src/dataBase/Networks.java
+++ src/dataBase/Networks.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 public class Networks {
 private ArrayList<A_Point> Networks=new ArrayList<A_Point>();
 
+
+public Networks(Networks networks) {
+	Networks = networks.getNetworks();
+}
+public Networks() {
+}
 public void addFromInput(String path){
 	Output2Networks a=new Output2Networks();
 	a.add(path, Networks);
@@ -12,6 +18,9 @@ public void addFromInput(String path){
 
 public ArrayList<A_Point> getNetworks() {
 	return Networks;
+}
+public int size(){
+	return Networks.size();
 }
 
 }
