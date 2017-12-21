aa36b2f9f592a502e442a0be3bc9febc019297f2
Student <Student@10.0.13.129>
2017-12-21 18:49:13 +0200

    algo 1 complete

--- src/algorithems/Algo1.java
+++ src/algorithems/Algo1.java
@@ -1,36 +1,54 @@
 package algorithems;
 
-import assignment.All_Networks;
-import assignment.FilterByMac;
+import java.util.ArrayList;
+
 import assignment.Point3D;
 import assignment.Signal_Comp2;
+import assignment.ToCsv;
+import dataBase.Networks;
 
 public class Algo1 {
+	private static ArrayList<MacPoint> ans=new  ArrayList<MacPoint>();
 	
+	public  void algo1full() {
+		Networks  Data=new Networks();
+		Data.addFromInput("C:/Users/Student/Desktop/_comb_all_BM2_.csv");
+		while(Data.size()>0) {
+				MacPoint temp=new MacPoint();
+				String mac=Data.getNetworks().get(0).getNets().get(0).getMac();
+				temp.setMac(mac);
+				temp.setLocation(algo1(mac));
+				ans.add(temp);
+				FilterByMacRev filtered=new FilterByMacRev();
+				filtered.filter(mac, Data);
+			}
+		print();
+	}
 	
+
 	
-	static String wantedMac="";
-	
-	public static Point3D algo1() {
+	private static Point3D algo1(String mac) {
 		Point3D w=new Point3D();
-		All_Networks nets=new All_Networks("ScanInput");
+		Networks  nets=new Networks();
+		nets.addFromInput("C:/Users/Student/Desktop/_comb_all_BM2_.csv");
 		FilterByMac filtered=new FilterByMac();
-		filtered.filter(wantedMac, nets);
+		filtered.filter(mac,  nets);
 		Signal_Comp2 comp= new Signal_Comp2();
-		nets.getNetworks().sort(comp);
+	//	nets.getNetworks().sort(comp);
 		double mone=0.0;
 		double mechane=0.0;
-		for(int i=0; i<nets.size(); i++) {
-			double koved=1/(nets.getNetworks().get(i).getSign()*nets.getNetworks().get(i).getSign());
+		for(int i=0; i<nets.size(); i++)
+			for(int j=0; j<nets.getNetworks().get(i).getNets().size(); j++){
+			double koved=1.0/(nets.getNetworks().get(i).getNets().get(j).getSign()*nets.getNetworks().get(i).getNets().get(j).getSign());
 			mone+=nets.getNetworks().get(i).getLocation().getAlt() *koved;
-			mechane+=koved;			
+			mechane+=koved;	
 		}
 		w.setAlt((mone/mechane));
-		
 		mone=0.0;
 		mechane=0.0;
-		for(int i=0; i<nets.size(); i++) {
-			double koved=1/(nets.getNetworks().get(i).getSign()*nets.getNetworks().get(i).getSign());
+		for(int i=0; i<nets.size(); i++)
+			for(int j=0; j<nets.getNetworks().get(i).getNets().size(); j++){
+			double koved=1.0/(nets.getNetworks().get(i).getNets().get(j).getSign()*nets.getNetworks().get(i).getNets().get(j).getSign());
 			mone+=nets.getNetworks().get(i).getLocation().getLon() *koved;
 			mechane+=koved;			
 		}
@@ -38,8 +56,9 @@ public class Algo1 {
 		
 		mone=0.0;
 		mechane=0.0;
-		for(int i=0; i<nets.size(); i++) {
-			double koved=1/(nets.getNetworks().get(i).getSign()*nets.getNetworks().get(i).getSign());
+		for(int i=0; i<nets.size(); i++)
+			for(int j=0; j<nets.getNetworks().get(i).getNets().size(); j++){
+			double koved=1.0/(nets.getNetworks().get(i).getNets().get(j).getSign()*nets.getNetworks().get(i).getNets().get(j).getSign());
 			mone+=nets.getNetworks().get(i).getLocation().getLat() *koved;
 			mechane+=koved;			
 		}
@@ -48,7 +67,20 @@ public class Algo1 {
 		return w;
 	}
 	
-	
+	private void print() {
+		ArrayList<ArrayList<String>> p=new ArrayList<ArrayList<String>>();
+		for(int i=0;i<ans.size();i++) {
+			ArrayList<String> s=new ArrayList<String>();
+			s.add(ans.get(i).getMac());
+			s.add(Double.toString(ans.get(i).getLocation().getLat()));
+			s.add(Double.toString(ans.get(i).getLocation().getLon()));
+			s.add(Double.toString(ans.get(i).getLocation().getAlt()));
+			p.add(s);
+		}
+			ToCsv print=new ToCsv("C:/Users/Student/Desktop/check.csv",p);
+			print.toCSV();
+			
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
