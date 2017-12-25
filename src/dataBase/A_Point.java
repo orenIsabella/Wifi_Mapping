8d88b80b87f8ff38451886aca40f556b9324e23e
arbel <arbel@DESKTOP-KJE213N>
2017-12-25 11:33:48 +0200

    add equals method

--- src/dataBase/A_Point.java
+++ src/dataBase/A_Point.java
@@ -12,81 +12,87 @@ import assignment.Point3D;
  *
  */
 public class A_Point {
-	
-private ArrayList<Network> nets=new ArrayList<Network>();
-private String Time,ID;
-private double weight;
-private Point3D location=new Point3D();
-/**
- * 
- * @return nets
- */
-public ArrayList<Network> getNets() {
-	return nets;
-}
-/**
- * @category sets nets
- * @param nets
- */
-public void setNets(ArrayList<Network> nets) {
-	this.nets = nets;
-}
-/**
- * 
- * @return time
- */
-public String getTime() {
-	return Time;
-}
-/**
- * @category sets time
- * @param time
- */
-public void setTime(String time) {
-	Time = time;
-}
-/**
- * 
- * @return id
- */
-public String getID() {
-	return ID;
-}
-/**
- * @category sets id
- * @param iD
- */
-public void setID(String iD) {
-	ID = iD;
-}
-/**
- * 
- * @return location
- */
-public Point3D getLocation() {
-	return location;
-}
-/**
- * @category sets location
- * @param location
- */
-public void setLocation(Point3D location) {
-	this.location = location;
-}
-/**
- * 
- * @return weight
- */
-public double getWeight() {
-	return weight;
-}
-/**
- * @category sets weight
- * @param weight
- */
-public void setWeight(double weight) {
-	this.weight = weight;
-}
 
+	private ArrayList<Network> nets=new ArrayList<Network>();
+	private String Time,ID;
+	private double weight;
+	private Point3D location=new Point3D();
+	/**
+	 * 
+	 * @return nets
+	 */
+	public ArrayList<Network> getNets() {
+		return nets;
+	}
+	/**
+	 * @category sets nets
+	 * @param nets
+	 */
+	public void setNets(ArrayList<Network> nets) {
+		this.nets = nets;
+	}
+	/**
+	 * 
+	 * @return time
+	 */
+	public String getTime() {
+		return Time;
+	}
+	/**
+	 * @category sets time
+	 * @param time
+	 */
+	public void setTime(String time) {
+		Time = time;
+	}
+	/**
+	 * 
+	 * @return id
+	 */
+	public String getID() {
+		return ID;
+	}
+	/**
+	 * @category sets id
+	 * @param iD
+	 */
+	public void setID(String iD) {
+		ID = iD;
+	}
+	/**
+	 * 
+	 * @return location
+	 */
+	public Point3D getLocation() {
+		return location;
+	}
+	/**
+	 * @category sets location
+	 * @param location
+	 */
+	public void setLocation(Point3D location) {
+		this.location = location;
+	}
+	/**
+	 * 
+	 * @return weight
+	 */
+	public double getWeight() {
+		return weight;
+	}
+	/**
+	 * @category sets weight
+	 * @param weight
+	 */
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	public boolean equals(A_Point a) {
+		if(a.getID()==this.ID&&
+				a.getLocation()==this.location&&
+				a.getTime()==this.Time)
+			return true;
+		else return false;
+	}
 
 }
