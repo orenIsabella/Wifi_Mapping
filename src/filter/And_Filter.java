482c31888a8325471b4afb7402679f906e85ce8d
Isabella Oren <Isabella Oren@LAPTOP-R6MH9MMA>
2017-12-27 22:23:54 +0200

    change function name

--- src/filter/And_Filter.java
+++ src/filter/And_Filter.java
@@ -3,7 +3,7 @@ package filter;
 import dataBase.Networks;
 
 public class And_Filter {
-	public void  Or(Filter a,Filter b,Networks nets) {
+	public void and(Filter a,Filter b,Networks nets) {
 		
 		a.filter(nets);
 		b.filter(nets);
