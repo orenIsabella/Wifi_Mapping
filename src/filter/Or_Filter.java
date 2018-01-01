e1249ae803c7780d8546e5960244a2c3a4d9d117
arbel <arbel@DESKTOP-KJE213N>
2018-01-01 18:13:01 +0200

    fixed

--- src/filter/Or_Filter.java
+++ src/filter/Or_Filter.java
@@ -16,11 +16,18 @@ public class Or_Filter implements Serializable{
 	public void  Or(Networks nets) {
 		Networks temp1=new Networks(nets);
 		Networks temp2=new Networks(nets);
+		System.out.println(temp1.size());
+		System.out.println(temp2.size());
 		Networks temp3=new Networks();
 		a.filter(temp1);
 		b.filter(temp2);
+		System.out.println(temp1.size());
+		System.out.println(temp2.size());
 		temp3.getNetworks().addAll(temp1.getNetworks());
 		temp3.getNetworks().addAll(temp2.getNetworks());
-		nets=temp3;
+		nets.getNetworks().clear();
+		nets.getNetworks().addAll(temp3.getNetworks());
+		System.out.println(nets.size());
+		
 	}
 }
