ba2dd6a83800e05350ea927da3d8a5f0dc0a3571
arbel <arbel@DESKTOP-KJE213N>
2018-01-03 21:20:42 +0200

    removed syso that was there for debuging

--- src/filter/Or_Filter.java
+++ src/filter/Or_Filter.java
@@ -6,7 +6,7 @@ public class Or_Filter implements Filter{
 
 	private Filter a, b;
 	private boolean flag=true; 
-	
+
 	public Or_Filter(Filter a, Filter b) {
 		this.a = a;
 		this.b = b;
@@ -16,20 +16,17 @@ public class Or_Filter implements Filter{
 	@Override
 	public void filter(Networks Data) {
 		if(flag) {
-		Networks temp1=new Networks(Data);
-		Networks temp2=new Networks(Data);
-		System.out.println(temp1.size());
-		System.out.println(temp2.size());
-		Networks temp3=new Networks();
-		a.filter(temp1);
-		b.filter(temp2);
-		System.out.println(temp1.size());
-		System.out.println(temp2.size());
-		temp3.getNetworks().addAll(temp1.getNetworks());
-		temp3.getNetworks().addAll(temp2.getNetworks());
-		Data.getNetworks().clear();
-		Data.getNetworks().addAll(temp3.getNetworks());
-		System.out.println(Data.size());
+			Networks temp1=new Networks(Data);
+			Networks temp2=new Networks(Data);
+			Networks temp3=new Networks();
+			a.filter(temp1);
+			b.filter(temp2);
+			System.out.println(temp1.size());
+			System.out.println(temp2.size());
+			temp3.getNetworks().addAll(temp1.getNetworks());
+			temp3.getNetworks().addAll(temp2.getNetworks());
+			Data.getNetworks().clear();
+			Data.getNetworks().addAll(temp3.getNetworks());
 		}
 		else {
 			Not_Filter g=new Not_Filter(a);
@@ -43,6 +40,6 @@ public class Or_Filter implements Filter{
 	public void setFlag(boolean flag) {
 		this.flag=flag;
 
-		
+
 	}
 }
