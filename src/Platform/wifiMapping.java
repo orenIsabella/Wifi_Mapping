b136be659985d0e7301c38c2cb0b859d18841cc0
Isabella Oren <Isabella Oren@LAPTOP-R6MH9MMA>
2018-01-04 15:29:38 +0200

    Changed the Gui and added the customized filter option to it.

--- src/Platform/wifiMapping.java
+++ src/Platform/wifiMapping.java
@@ -13,15 +13,34 @@ import java.awt.Font;
 import javax.swing.SwingConstants;
 
 import algorithems.Algo2;
+import assignment.CreateKML;
 import assignment.ToCsv;
 import dataBase.Networks;
 import dataBase.toArrayList;
+import javax.swing.JTextPane;
+import javax.swing.JSeparator;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.JCheckBox;
 
 public class wifiMapping {
 
 	private JFrame frame;
 	protected Networks net;
-	public String customFilter;
+	public String custom;
+	protected String destinationFolder;
+	public int filtersUsed;
+	protected int files;
+	private int devices;
+	private JTextField textField;
+	private JTextField txtMin;
+	private JTextField txtMax;
+	private JTextField txtMinLat;
+	private JTextField txtMaxLat;
+	private JTextField txtMinLon;
+	private JTextField txtMaxLon;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -51,7 +70,7 @@ public class wifiMapping {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 640, 616);
+		frame.setBounds(100, 100, 443, 975);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
@@ -66,7 +85,7 @@ public class wifiMapping {
 				
 			}
 		});
-		btnAddFoldersName.setBounds(22, 24, 167, 43);
+		btnAddFoldersName.setBounds(22, 28, 167, 43);
 		frame.getContentPane().add(btnAddFoldersName);
 
 		JButton btnNewButton = new JButton("Add CSV");
@@ -78,17 +97,18 @@ public class wifiMapping {
 				net.addFromWiggle(csvFileName);
 			}
 		});
-		btnNewButton.setBounds(22, 91, 167, 37);
+		btnNewButton.setBounds(22, 95, 167, 37);
 		frame.getContentPane().add(btnNewButton);
 
 		JButton btnNewButton_1 = new JButton("Delete All");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				net.deleteAllData();
+				
 			}
 		});
 		btnNewButton_1.setBackground(new Color(153, 204, 204));
-		btnNewButton_1.setBounds(22, 152, 167, 37);
+		btnNewButton_1.setBounds(22, 165, 167, 37);
 		frame.getContentPane().add(btnNewButton_1);
 
 		JButton btnNewButton_2 = new JButton("Save to CSV");
@@ -96,41 +116,48 @@ public class wifiMapping {
 			public void actionPerformed(ActionEvent e) {
 				//here we need to save all our database to a combined csv file
 				String destinationFolder="";
-			//	ArrayList<ArrayList<String>> help=new ArrayList<ArrayList<String>>();
-			//	help.add(net.getNetworks().
 				toArrayList help=new toArrayList(destinationFolder, net);
 				help.toCSV();
 				
 			}
 		});
 		btnNewButton_2.setBackground(new Color(153, 204, 204));
-		btnNewButton_2.setBounds(22, 210, 167, 37);
+		btnNewButton_2.setBounds(22, 226, 167, 37);
 		frame.getContentPane().add(btnNewButton_2);
 
 		JButton btnSaveToKml = new JButton("Save to KML");
 		btnSaveToKml.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//here we need to save all our database to a combined KML file
+			//	toArrayList h=new toArrayList(destinationFolder, net);
+				CreateKML n=new CreateKML(net,destinationFolder);
 			}
 		});
 		btnSaveToKml.setBackground(new Color(153, 204, 204));
-		btnSaveToKml.setBounds(22, 267, 167, 37);
+		btnSaveToKml.setBounds(22, 279, 167, 37);
 		frame.getContentPane().add(btnSaveToKml);
 
 		JButton btnShowData = new JButton("Show Data");
 		btnShowData.addActionListener(new ActionListener() {
+			
+
 			public void actionPerformed(ActionEvent e) {
 				//we need to print/show the data we have in this format: 
 				//filters used, how many files, how many devices.
+				files=net.size();
+			Component parentComponent;
+				//	devices=net.getNetworks().
+				JOptionPane.showConfirmDialog(parentComponent, "the amount of filters used is: "+ filtersUsed + "There are "+ files+" files. "+"There are "+ devices+ " devices. ");
 			}
 		});
 		btnShowData.setBackground(new Color(153, 204, 204));
-		btnShowData.setBounds(22, 322, 167, 37);
+		btnShowData.setBounds(22, 334, 167, 37);
 		frame.getContentPane().add(btnShowData);
 
 		JButton btnFilterByTime = new JButton("Filter by Time");
 		btnFilterByTime.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				filtersUsed++;
 				String min = null;
 				JOptionPane.showInputDialog("Please enter the minimum time you want to filter from", min);
 				String max = null;
@@ -146,6 +173,7 @@ public class wifiMapping {
 		JButton btnFilterByLocation = new JButton("Filter by Location");
 		btnFilterByLocation.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
 				String minlat = null;
 				JOptionPane.showInputDialog("Please enter the minimum lat you want to filter from", minlat);
 				String maxlat = null;
@@ -169,6 +197,7 @@ public class wifiMapping {
 		JButton btnFilterByDevice = new JButton("Filter by Device");
 		btnFilterByDevice.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
 				String deviceName = null;
 				JOptionPane.showInputDialog("Please enter the device name you want to filter the data by", deviceName);
 				//here we need to add a filter by the deviceName we recieved
@@ -182,6 +211,7 @@ public class wifiMapping {
 		JButton btnUndoTheFilter = new JButton("Undo the filter");
 		btnUndoTheFilter.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
 				//here we need to undo the last filter
 			}
 		});
@@ -193,6 +223,7 @@ public class wifiMapping {
 		JButton btnSavefilter = new JButton("Save Filter");
 		btnSavefilter.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				filtersUsed++;
 				//here we need to update our database and save it by the filter
 			}
 		});
@@ -219,7 +250,7 @@ public class wifiMapping {
 			}
 		});
 		btnAlgo.setBackground(new Color(255, 204, 255));
-		btnAlgo.setBounds(408, 47, 167, 125);
+		btnAlgo.setBounds(22, 395, 167, 125);
 		frame.getContentPane().add(btnAlgo);
 
 		JButton btnAlgo_1 = new JButton("Algo2");
@@ -281,7 +312,141 @@ public class wifiMapping {
 			}
 		});
 		btnAlgo_1.setBackground(new Color(255, 204, 255));
-		btnAlgo_1.setBounds(408, 199, 167, 137);
+		btnAlgo_1.setBounds(211, 395, 167, 125);
 		frame.getContentPane().add(btnAlgo_1);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(22, 377, 371, 30);
+		frame.getContentPane().add(separator);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(22, 537, 371, 2);
+		frame.getContentPane().add(separator_1);
+		
+		JLabel lblCustomizeTheFilter = new JLabel("Customize the Filter:");
+		lblCustomizeTheFilter.setBounds(22, 544, 356, 29);
+		frame.getContentPane().add(lblCustomizeTheFilter);
+		
+		JLabel lblNot = new JLabel("NOT");
+		lblNot.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNot.setBounds(22, 575, 36, 29);
+		frame.getContentPane().add(lblNot);
+		
+		JRadioButton radioButton = new JRadioButton("");
+		radioButton.setBounds(22, 624, 36, 29);
+		frame.getContentPane().add(radioButton);
+		
+		JRadioButton radioButton_1 = new JRadioButton("");
+		radioButton_1.setBounds(22, 680, 36, 29);
+		frame.getContentPane().add(radioButton_1);
+		
+		JRadioButton radioButton_2 = new JRadioButton("");
+		radioButton_2.setBounds(18, 756, 36, 29);
+		frame.getContentPane().add(radioButton_2);
+		
+		JRadioButton radioButton_3 = new JRadioButton("");
+		radioButton_3.setBounds(22, 842, 36, 29);
+		frame.getContentPane().add(radioButton_3);
+		
+		JLabel lblId = new JLabel("ID");
+		lblId.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblId.setBounds(74, 597, 29, 30);
+		frame.getContentPane().add(lblId);
+		
+		textField = new JTextField();
+		textField.setBounds(58, 627, 115, 22);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblTime = new JLabel("Time");
+		lblTime.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblTime.setBounds(68, 651, 104, 29);
+		frame.getContentPane().add(lblTime);
+		
+		txtMin = new JTextField();
+		txtMin.setText("min");
+		txtMin.setBounds(58, 675, 56, 34);
+		frame.getContentPane().add(txtMin);
+		txtMin.setColumns(10);
+		
+		txtMax = new JTextField();
+		txtMax.setText("max");
+		txtMax.setBounds(117, 675, 56, 34);
+		frame.getContentPane().add(txtMax);
+		txtMax.setColumns(10);
+		
+		JLabel lblLocation = new JLabel("Location");
+		lblLocation.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblLocation.setBounds(58, 724, 66, 29);
+		frame.getContentPane().add(lblLocation);
+		
+		txtMinLat = new JTextField();
+		txtMinLat.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		txtMinLat.setText("min lat");
+		txtMinLat.setBounds(58, 756, 56, 30);
+		frame.getContentPane().add(txtMinLat);
+		txtMinLat.setColumns(10);
+		
+		txtMaxLat = new JTextField();
+		txtMaxLat.setText("max lat");
+		txtMaxLat.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		txtMaxLat.setColumns(10);
+		txtMaxLat.setBounds(117, 756, 66, 30);
+		frame.getContentPane().add(txtMaxLat);
+		
+		txtMinLon = new JTextField();
+		txtMinLon.setText("min lon");
+		txtMinLon.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		txtMinLon.setColumns(10);
+		txtMinLon.setBounds(58, 788, 56, 30);
+		frame.getContentPane().add(txtMinLon);
+		
+		txtMaxLon = new JTextField();
+		txtMaxLon.setText("max lon");
+		txtMaxLon.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		txtMaxLon.setColumns(10);
+		txtMaxLon.setBounds(117, 788, 66, 30);
+		frame.getContentPane().add(txtMaxLon);
+		
+		JCheckBox chckbxUse = new JCheckBox("");
+		chckbxUse.setBounds(194, 620, 36, 37);
+		frame.getContentPane().add(chckbxUse);
+		
+		JCheckBox checkBox = new JCheckBox("");
+		checkBox.setBounds(194, 675, 36, 37);
+		frame.getContentPane().add(checkBox);
+		
+		JCheckBox checkBox_1 = new JCheckBox("");
+		checkBox_1.setBounds(194, 756, 36, 37);
+		frame.getContentPane().add(checkBox_1);
+		
+		JLabel lblUploadFromSaved = new JLabel("upload from saved files");
+		lblUploadFromSaved.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblUploadFromSaved.setBounds(58, 815, 140, 29);
+		frame.getContentPane().add(lblUploadFromSaved);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(58, 842, 123, 30);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JCheckBox checkBox_2 = new JCheckBox("");
+		checkBox_2.setBounds(194, 839, 36, 37);
+		frame.getContentPane().add(checkBox_2);
+		
+		JCheckBox chckbxOr = new JCheckBox("OR");
+		chckbxOr.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		chckbxOr.setBounds(244, 593, 71, 37);
+		frame.getContentPane().add(chckbxOr);
+		
+		JCheckBox chckbxAnd = new JCheckBox("AND");
+		chckbxAnd.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		chckbxAnd.setBounds(313, 593, 84, 37);
+		frame.getContentPane().add(chckbxAnd);
+		
+		JButton btnApply = new JButton("APPLY");
+		btnApply.setBackground(new Color(255, 255, 153));
+		btnApply.setBounds(238, 651, 155, 37);
+		frame.getContentPane().add(btnApply);
 	}
 }
