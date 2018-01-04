18d68951362cfa13d1ff892700963858a69d1444
Isabella Oren <Isabella Oren@LAPTOP-R6MH9MMA>
2018-01-04 13:33:42 +0200

    gui

--- src/Platform/wifiMapping.java
+++ src/Platform/wifiMapping.java
@@ -51,7 +51,7 @@ public class wifiMapping {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 639, 462);
+		frame.setBounds(100, 100, 640, 616);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
